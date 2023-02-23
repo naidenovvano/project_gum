350456fa613b938ac643f71ebb9344db04624c18
Burd <ivannaidenov95@gmail.com>
2023-02-23 13:51:12 +0200

    init commit 1

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// Burger menu
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const menuCloseItem = document.querySelector('.header-nav-cross');
+    const menuLinks = document.querySelectorAll('.header-link')
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    });
+    if (window.innerWidth <= 768) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header-nav-active');
+            });
+        }
+    }
+}());
+
+// Scroll to anchors
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
+// Scroll bar
+function scrollBar() {
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = scroll / height * 100;
+
+    document.getElementById('scrollBar').style.width = scrolled + '%';
+}
+
+window.addEventListener('scroll', scrollBar);
